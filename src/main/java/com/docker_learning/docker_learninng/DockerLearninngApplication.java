67e67ff1602cf9bc6d338a3e758af01d0f698f3f
KingEddie01 <edk6004@gmail.com>
2024-04-05 10:09:19 +0100

    first commit

---
+++ src/main/java/com/docker_learning/docker_learninng/DockerLearninngApplication.java
@@ -0,0 +1,13 @@
+package com.docker_learning.docker_learninng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLearninngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerLearninngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker_learning/docker_learninng/DockerLearninngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker_learning.docker_learninng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerLearninngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
